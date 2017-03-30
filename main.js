559ce4120633fc3c75635dc60ca7ee43633ee283
0xB4DC0FF33 <skullpand4@gmail.com>
2017-03-30 11:43:08 +0200

    Code Reorganization

--- main.js
+++ main.js
@@ -1,85 +1,41 @@
 var Discord = require("discord.js");
+var nabot = require('./nabot.js');
 
-var $;
-require("jsdom").env("", function(err, window) {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	$ = require("jquery")(window);
+var config = require("./config.json");
+
+// Variables initialization
+const bot = new Discord.Client();
+nabot.setBot(bot);
+
+bot.on("message", function(message){
+  var msg = message.toString();
+  // If the prefix is matching
+  if (msg.charAt(0) == config.prefix) {
+    cmd = msg.substr(1).split(' ')[0];
+    var args = msg.substr(1).split(' ');
+    // We need to clean the args from empty strings
+    for (var i = 0; i < args.length; i++) {
+      if (args[i] == '') {
+        args.splice(i, 1);
+        i--;
+      }
+    }
+    args.shift();
+    // We print the command and the author in the server's console
+    console.log('######################');
+    console.log('User : '+message.author.username);
+    console.log('Command : '+cmd);
+    console.log('Arguments : '+args.toString());
+
+    nabot.setArgs(args);
+    nabot.setMsg(msg);
+    var response = nabot[cmd](function(res){
+      console.log('Response : '+res);
+      message.channel.send(res);
+    });
+    console.log('######################');
+  }
 });
 
-var NaBot = new Discord.Client();
-
-const token = '*****************************************';    // Bot's API token
-
-const fullMaps = new Array( "Dust", "Dust II", "Nuke", "Aztec", "Cobblestone", "Overpass", "Mirage", "Inferno", "Cache", "Office", // Full CS:GO map list
-"Vertigo", "Assault", "Italy", "Militia", "Train", "Coast", "Cruise", "Mikla", "Empire", "Santorini", "Royal", "Tulip");
-
- 
-NaBot.on("message", function(message){
-  var command = message.toString().split(' ');
-	
-	if(command[0] === "$ping") {                                            // Test message, to see if the bot successfully read messages
-	  NaBot.reply(message, "OVER 9000 ! bien ta grotte ? :D");
-	}else if(command[0] === "$testMaps") {                                  // getMaps function test command
-	  NaBot.reply(message, getMaps(5));
-	}else if(command[0] === "$maps"){                        		// Main command => Get randoms CS:GO maps
-	  NaBot.reply(message, getMaps(parseInt(command[1], 10)).toString());
-	}else if(command[0] === "$stats"){
-	  NaBot.reply(message, getStats(parseInt(command[1], 10)).toString());
-	}
-	
-});
-
- /**
-  * FUNCTION GET MAPS
-  * 
-  * Will return random maps from CS:GO
-  * 
-  * Args = int = Number of wanted maps
-  * Return  = str = Message to print
-  *
-  **/
-
-function getMaps(number) {
-  console.log("function getMaps called with args : number = "+number);
-  var remainMaps = fullMaps;
-  var mapToPrint = new Array();
-  
-  for (var i = 0; i < number; i++) {
-    var random = Math.floor(Math.random()*remainMaps.length);
-    var map = remainMaps[random];
-    
-    mapToPrint.push(map);
-    remainMaps.splice(remainMaps.indexOf(map), 1);
-  }
-  var str = "";
-  for (var i = 1; i <= mapToPrint.length; i++) {
-    str += "\n\tMap n°"+i+" : "+mapToPrint[i-1];
-  }
-  console.log(str);
-  return str;
-}
-
-function getStats(UID) {
-  var APIkey = '0F8BE87F0091E9A22CD6D4CA3EF6A5E7';
-  var req = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key='+APIkey+'&steamid='+UID;
-  var response = '';
-  var json = $.getJSON(req, function (json) {
-    var timePlayed = parseInt((json.total_time_played/60)/60, 10); // CONVERT SECONDS IN HOURS
-	  var totalKills = json.total_kills;
-  	var totalDeaths = json.total_deaths;
-  	var ratio = parseInt(totalKills/totalDeaths, 10);
-  	console.log('lalalala');
-  	response = "GLOBAL STATS :\n\tKills: "+totalKills+"\n\tDeaths: "+totalDeaths+"\n\tRatio: "+ratio+"\n\tTime played:"+timePlayed;
-  }).fail(function() {
-    console.log( "error" );
-  });
-  return response;
-}
-
-NaBot.loginWithToken(token);
-
-
-
+bot.login(config.token);
+console.log("Nabot successfully connected with token : \n"+config.token);
